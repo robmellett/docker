c49385dc3af6ab78605200aca47dcf933630d13c
Rob <dev@robmellett.com>
2020-04-22 22:03:42 +1000

    Cleanup

--- src/docker.sh
+++ src/docker.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-# Find container names with `docker container ls`
-
-echo ">> Database IP:"
-echo $(docker inspect docker-database | grep IPAddress)
-
 # SSH into Web App
 docker exec -it --user ubuntu acme-app bash
