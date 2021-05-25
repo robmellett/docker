6ece5e61747e29660e3f038308d8b04beb95dd3b
Rob <dev@robmellett.com>
2021-05-26 08:24:43 +1000

    Own home directory

--- src/services/setup-web.sh
+++ src/services/setup-web.sh
@@ -70,6 +70,11 @@ else
     exit 1
 fi
 
+# Setup tinker directory
+mkdir -p /home/ubuntu/.config
+chown -R ubuntu: /home/ubuntu/
+chown -R ubuntu: /home/ubuntu/.config
+
 # Docker needs this line before it can run
 # the PHP-FPM service
 mkdir -p /run/php
