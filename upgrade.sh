0ea615c6db7e4aab386ebb9c6c6a47349848bfc2
Rob Mellett <dev@robmellett.com>
2022-02-18 08:42:55 +0700

    Fix upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -2,8 +2,10 @@
 
 git clone https://github.com/laravel/sail temp
 
-cp temp/runtimes/7.4 src/php74
+cp temp/runtimes/7.4/* src/php74
 
-cp temp/runtimes/8.0 src/php80
+cp temp/runtimes/8.0/* src/php80
 
-cp temp/runtimes/8.1 src/php81
+cp temp/runtimes/8.1/* src/php81
+
+rm -rf temp/
