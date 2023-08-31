e3c67e5783fd4e86748f04fca21b787ef11d8abe
Rob Mellett <dev@robmellett.com>
2023-08-31 09:33:16 +0700

    Try this

--- upgrade.sh
+++ upgrade.sh
@@ -11,11 +11,3 @@ cp runtimes/8.0/* src/php80
 cp runtimes/8.1/* src/php81
 
 rm -rf cd laravel-sail
-
-# git add .
-
-# git commit -m 'Laravel Sail Bump'
-
-# git push -u origin "feature/laravel-sail-upgrade"
-
-# # git request-pull "origin/master" "feature/laravel-sail-upgrade"
