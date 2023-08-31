f696919adad131a6f537c8ca3aacef5a89769e54
Rob Mellett <dev@robmellett.com>
2023-08-31 09:25:04 +0700

    Fix script

--- upgrade.sh
+++ upgrade.sh
@@ -2,11 +2,11 @@
 
 cd laravel-sail
 
-cp temp/runtimes/7.4/* src/php74
+cp runtimes/7.4/* src/php74
 
-cp temp/runtimes/8.0/* src/php80
+cp runtimes/8.0/* src/php80
 
-cp temp/runtimes/8.1/* src/php81
+cp runtimes/8.1/* src/php81
 
 rm -rf cd laravel-sail
 
